cd2ea0deae851755f297b48cec56c38acd48ab1a
PatolinoOMag0 <161628377+PatolinoOMag0@users.noreply.github.com>
2025-02-28 16:59:58 -0300

    Foi

---
+++ IW-II__2025/IW-II__2025/Aula_02/exemplo06.php
@@ -0,0 +1,15 @@
+<?php
+    $nome = "Matheus";
+    $idade = 18;
+    $funcao = "Trader";
+
+    $frase = "Dados Pessoais: ";
+    $frase .= $nome;
+    $frase .= " - ";
+    $frase .= $idade;
+    $frase .= " - ";
+    $frase .= $funcao;
+
+    echo $frase;
+
+?>
