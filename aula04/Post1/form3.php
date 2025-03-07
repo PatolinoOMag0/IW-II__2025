c9c715ebc714506a8a0ae977a0608ae6539d439c
PatolinoOMag0 <161628377+PatolinoOMag0@users.noreply.github.com>
2025-03-07 17:01:32 -0300

    Aula04

---
+++ aula04/Post1/form3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de Login</title>
+</head>
+<body>
+    <form action="exe3.php" method="POST">
+        <p>Digite Login e Senha para acessar a ETEC</p>
+        <input type="text" id="login" name="cxlog" required>
+
+        <input type="password" id="senha" name="cxsen" required>
+
+        <input type="submit" value="Acessar a ETEC">
+    </form>
+</body>
+</html>
